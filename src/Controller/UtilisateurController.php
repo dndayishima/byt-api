bb880a2e53faf0c47b9e2c311236dc03dc63dc89
Divin Ndayishima <divin.ndayishima@outlook.com>
2019-01-20 20:04:26 +0100

    Inscription + Connexion d'un utilisateur

---
+++ src/Controller/UtilisateurController.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+use App\Entity\Utilisateur;
+
+use \DateTime;
+
+class UtilisateurController extends AbstractController
+{
+    /**
+     * @Route("/utilisateur", name="utilisateur")
+     * Création d'un utilisateur (inscription)
+     * Requête de type POST
+     * @return id de l'utilisateur inscrit
+     */
+    public function createUtilisateur(Request $request) {
+
+        // Création d'un utilisateur en considérant que la validité des champs
+        // a été vérifiée du côté du front-end
+
+        $entityManager = $this->getDoctrine()->getManager();
+
+        // Conversion du JSON en tableau associtif PHP
+        $data = json_decode($request->getContent(), true);
+
+        $naissance = new DateTime($data["naissance"]);
+
+        $utilisateur = new Utilisateur();
+        $utilisateur->setUsername($data["username"]);
+        $utilisateur->setPassword($data["password"]);
+        $utilisateur->setNom($data["nom"]);
+        $utilisateur->setPrenom($data["prenom"]);
+        $utilisateur->setNaissance($naissance);
+
+        // Avant d'insérer un nouvel utilisateur dans la base de données
+        // on vérifie que le nom d'utilisateur est unique.
+        // C'est une donnée qui sera utilisée pour la connexion
+        // donc il faut qu'elle soit unique.
+        $repository = $this->getDoctrine()->getRepository(Utilisateur::class);
+        $utilisateur_test = $repository->findOneBy([
+            "username" => $data["username"]
+        ]);
+
+        $result = array();
+
+        if ($utilisateur_test === NULL) { // donc il est unique
+            $entityManager->persist($utilisateur);
+
+            $entityManager->flush();
+
+            // Après insertion du nouvel utilisateur dans la
+            // base de données, retourner son ID
+
+            $repository = $this->getDoctrine()->getRepository(Utilisateur::class);
+            $utilisateur = $repository->findOneBy([
+                "username" => $data["username"],
+                "nom" => $data["nom"],
+                "prenom" => $data["prenom"],
+                "naissance" => $naissance
+            ]);
+            
+            $result["id"] = $utilisateur->getId();
+            $response = new JsonResponse($result);
+        } else {
+            $result["message"] = "Ce nom d'utilisateur existe déjà";
+            $response = new JsonResponse($result, 419);
+        }
+
+        return $response;
+    }
+
+    /**
+     * @Route("/utilisateur/{id}", name="get_utilisateur")
+     * Information sur un utilisateur
+     * Requête de type GET
+     * @return Utilisateur
+     */
+    public function getUtilisateur($id) {
+        $repository = $this->getDoctrine()->getRepository(Utilisateur::class);
+        $utilisateur = $repository->find($id);
+        $response = array();
+        if ($utilisateur !== NULL) {
+            
+            $response["id"] = $utilisateur->getId();
+            $response["username"] = $utilisateur->getUsername();
+            $response["password"] = "";
+            $response["nom"] = $utilisateur->getNom();
+            $response["prenom"] = $utilisateur->getPrenom();
+
+            $naissance = $utilisateur->getNaissance();
+            $naissance = $naissance->format("Y-m-d");
+            $response["naissance"] = $naissance;
+            
+            $response["telephone1"] = ($utilisateur->getTelephone1() === NULL ? 0 : $utilisateur->getTelephone1());
+            $response["telephone2"] = ($utilisateur->getTelephone2() === NULL ? 0 : $utilisateur->getTelephone2());
+            $response["telephone3"] = ($utilisateur->getTelephone3() === NULL ? 0 : $utilisateur->getTelephone3());
+
+        }
+        return new JsonResponse($response);
+    }
+
+
+    /**
+     * @Route("/utilisateur/{username}/{password}", name="connexion")
+     * Connextion d'un utilisateur
+     * Requête de type GET 
+     * @return Utilisateur
+     */
+    public function connexion($username, $password) {
+        $repository = $this->getDoctrine()->getRepository(Utilisateur::class);
+        $utilisateur = $repository->findOneBy([
+            "username" => $username,
+            "password" => $password
+        ]);
+        $response = array();
+        if ($utilisateur !== NULL) {
+            
+            $response["id"] = $utilisateur->getId();
+            $response["username"] = $utilisateur->getUsername();
+            $response["password"] = "";
+            $response["nom"] = $utilisateur->getNom();
+            $response["prenom"] = $utilisateur->getPrenom();
+
+            $naissance = $utilisateur->getNaissance();
+            $naissance = $naissance->format("Y-m-d");
+            $response["naissance"] = $naissance;
+            
+            $response["telephone1"] = ($utilisateur->getTelephone1() === NULL ? 0 : $utilisateur->getTelephone1());
+            $response["telephone2"] = ($utilisateur->getTelephone2() === NULL ? 0 : $utilisateur->getTelephone2());
+            $response["telephone3"] = ($utilisateur->getTelephone3() === NULL ? 0 : $utilisateur->getTelephone3());
+
+        }
+        return new JsonResponse($response);
+    }
+}
---
+++ src/Entity/Utilisateur.php
@@ -0,0 +1,161 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\UtilisateurRepository")
+ */
+class Utilisateur
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=40)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $prenom;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $naissance;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $telephone1;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $telephone2;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $telephone3;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): self
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getNaissance(): ?\DateTimeInterface
+    {
+        return $this->naissance;
+    }
+
+    public function setNaissance(\DateTimeInterface $naissance): self
+    {
+        $this->naissance = $naissance;
+
+        return $this;
+    }
+
+    public function getTelephone1(): ?int
+    {
+        return $this->telephone1;
+    }
+
+    public function setTelephone1(?int $telephone1): self
+    {
+        $this->telephone1 = $telephone1;
+
+        return $this;
+    }
+
+    public function getTelephone2(): ?int
+    {
+        return $this->telephone2;
+    }
+
+    public function setTelephone2(?int $telephone2): self
+    {
+        $this->telephone2 = $telephone2;
+
+        return $this;
+    }
+
+    public function getTelephone3(): ?int
+    {
+        return $this->telephone3;
+    }
+
+    public function setTelephone3(?int $telephone3): self
+    {
+        $this->telephone3 = $telephone3;
+
+        return $this;
+    }
+}
