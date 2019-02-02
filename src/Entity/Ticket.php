d2272c7b2bfc08b3d01c7a80025979035ae59ff0
Divin Ndayishima <divin.ndayishima@outlook.com>
2019-02-02 06:53:22 +0100

    Entity Ticket

---
+++ src/Entity/Ticket.php
@@ -0,0 +1,161 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\TicketRepository")
+ */
+class Ticket
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=40)
+     */
+    private $client;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $vendeur;
+
+    /**
+     * @ORM\Column(type="string", length=13)
+     */
+    private $numClient;
+
+    /**
+     * @ORM\Column(type="string", length=13)
+     */
+    private $numVendeur;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $prix;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $evenementId;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $dateAchat;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $dateEvenement;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getClient(): ?string
+    {
+        return $this->client;
+    }
+
+    public function setClient(string $client): self
+    {
+        $this->client = $client;
+
+        return $this;
+    }
+
+    public function getVendeur(): ?string
+    {
+        return $this->vendeur;
+    }
+
+    public function setVendeur(string $vendeur): self
+    {
+        $this->vendeur = $vendeur;
+
+        return $this;
+    }
+
+    public function getNumClient(): ?string
+    {
+        return $this->numClient;
+    }
+
+    public function setNumClient(string $numClient): self
+    {
+        $this->numClient = $numClient;
+
+        return $this;
+    }
+
+    public function getNumVendeur(): ?string
+    {
+        return $this->numVendeur;
+    }
+
+    public function setNumVendeur(string $numVendeur): self
+    {
+        $this->numVendeur = $numVendeur;
+
+        return $this;
+    }
+
+    public function getPrix(): ?float
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(float $prix): self
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getEvenementId(): ?int
+    {
+        return $this->evenementId;
+    }
+
+    public function setEvenementId(int $evenementId): self
+    {
+        $this->evenementId = $evenementId;
+
+        return $this;
+    }
+
+    public function getDateAchat(): ?\DateTimeInterface
+    {
+        return $this->dateAchat;
+    }
+
+    public function setDateAchat(\DateTimeInterface $dateAchat): self
+    {
+        $this->dateAchat = $dateAchat;
+
+        return $this;
+    }
+
+    public function getDateEvenement(): ?\DateTimeInterface
+    {
+        return $this->dateEvenement;
+    }
+
+    public function setDateEvenement(\DateTimeInterface $dateEvenement): self
+    {
+        $this->dateEvenement = $dateEvenement;
+
+        return $this;
+    }
+}
---
+++ src/Migrations/Version20190202055025.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190202055025 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE ticket (id INT AUTO_INCREMENT NOT NULL, client VARCHAR(40) NOT NULL, vendeur VARCHAR(50) NOT NULL, num_client VARCHAR(13) NOT NULL, num_vendeur VARCHAR(13) NOT NULL, prix DOUBLE PRECISION NOT NULL, evenement_id INT NOT NULL, date_achat DATE NOT NULL, date_evenement DATE NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE ticket');
+    }
+}
