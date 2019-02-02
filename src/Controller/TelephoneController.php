e5bb4eebf6d8e9448e98eadd09b637448480fddb
Divin Ndayishima <divin.ndayishima@outlook.com>
2019-02-02 11:12:02 +0100

    Gestion de l'achat de ticket

--- src/Controller/TelephoneController.php
+++ src/Controller/TelephoneController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 //use Symfony\Component\HttpFoundation\Response;
-//use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 use App\Entity\Telephone;
@@ -28,5 +28,39 @@ class TelephoneController extends AbstractController
             $response["credit"] = $telephone->getCredit();
         }
         return new JsonResponse($response);
+    }
+
+    /**
+     * @Route("/transaction", name="transaction")
+     * Requête de type POST
+     * @return boolean
+     */
+    public function transaction(Request $request) {
+        $data = json_decode($request->getContent(), true);
+        $repository = $this->getDoctrine()->getRepository(Telephone::class);
+        $src = $repository->findOneBy([
+            "num" => $data["src"]
+        ]);
+        $dest = $repository->findOneBy([
+            "num" => $data["dest"]
+        ]);
+        if (($src === NULL) || ($dest === NULL)) {
+            $result["message"] = "Source ou destinataire non existant dans la base de données";
+            return new JsonResponse($result, 419);
+        }
+        if ($data["amount"] > $src->getCredit()) {
+            $result["message"] = "Crédit insuffisant pour effectuer cette transaction";
+            return new JsonResponse($result, 419);
+        }
+        $src->setCredit($src->getCredit() - $data["amount"]);
+        $dest->setCredit($dest->getCredit() + $data["amount"]);
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($src);
+        $entityManager->flush();
+        $entityManager->persist($dest);
+        $entityManager->flush();
+
+        return new JsonResponse(true);
     }
 }
---
+++ src/Controller/TicketController.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+use App\Entity\Ticket;
+
+use \DateTime;
+
+class TicketController extends AbstractController
+{
+    /**
+     * @Route("/ticket", name="ticket")
+     * Création d'un ticket
+     * Requête de type POST
+     */
+    public function createTicket(Request $request) {
+
+        // Attention : Est-ce qu'une même personne peut acheter un ticket 2 fois ?
+        // Pour l'instant bloquer ceci côté front-end
+
+        // Tous les champs sont obligatoires pour la création d'un ticket
+        $entityManager = $this->getDoctrine()->getManager();
+
+        // Conversion du JSON en tableau associatif PHP
+        $data = json_decode($request->getContent(), true);
+
+        $result = array();
+        if (!$this->valideTicket($data)) {
+            $result["message"] = "Un champ important n'est pas renseigné dans les données !";
+            return new JsonResponse($result, 419);
+        } else {
+            $ticket = new Ticket();
+            $ticket->setClient($data["client"]);
+            $ticket->setVendeur($data["vendeur"]);
+            $ticket->setNumClient($data["numClient"]);
+            $ticket->setNumVendeur($data["numVendeur"]);
+            $ticket->setPrix($data["prix"]);
+            $ticket->setEvenementId($data["evenementId"]);
+            $dateAchat = new DateTime();
+            $ticket->setDateAchat($dateAchat);
+            $dateEvenement = new DateTime($data["dateEvenement"]);
+            $ticket->setDateEvenement($dateEvenement);
+
+            $entityManager->persist($ticket);
+            $entityManager->flush();
+        }
+
+        // Après insertion d'un nouveau ticket dans la base de données,
+        // retourner cet objet au complet
+
+        $repository = $this->getDoctrine()->getRepository(Ticket::class);
+        $ticket = $repository->findOneBy([
+            "client" => $data["client"],
+            "vendeur" => $data["vendeur"],
+            "numClient" => $data["numClient"],
+            "numVendeur" => $data["numVendeur"],
+            "prix" => $data["prix"],
+            "evenementId" => $data["evenementId"],
+            "dateAchat" => $dateAchat,
+            "dateEvenement" => $dateEvenement
+        ]);
+
+        if ($ticket !== NULL) {
+            $result["id"] = $ticket->getId();
+            $result["client"] = $ticket->getClient();
+            $result["vendeur"] = $ticket->getVendeur();
+            $result["numClient"] = $ticket->getNumClient();
+            $result["numVendeur"] = $ticket->getNumVendeur();
+            $result["prix"] = $ticket->getPrix();  
+            $result["evenementId"] = $ticket->getEvenementId();
+            $result["dateAchat"] = $ticket->getDateAchat()->format("Y-m-d");
+            $result["dateEvenement"] = $ticket->getDateEvenement()->format("Y-m-d");
+        }      
+        return new JsonResponse($result);
+    }
+
+    /**
+     * Vérification de la validité de tous les champs
+     * On vérifie s'ils sont renseigné
+     * @return boolean
+     */
+    public function valideTicket($data) {
+        return (
+            isset($data["client"]) &&
+            isset($data["vendeur"]) &&
+            isset($data["numClient"]) &&
+            isset($data["numVendeur"]) &&
+            isset($data["prix"]) &&
+            isset($data["evenementId"]) &&
+            isset($data["dateEvenement"])
+        );
+    }
+
+    /**
+     * @Route("/bought", name="bought")
+     * Vérifier si un ticket a été acheté par un utilisateur donné
+     * Requête de type POST
+     * @return boolean
+     */
+    public function bought(Request $request) {
+        $data = json_decode($request->getContent(), true);
+        $repository = $this->getDoctrine()->getRepository(Ticket::class);
+        $dateEvenement = new DateTime($data["dateEvenement"]);
+        $ticket = $repository->findOneBy([
+            "client" => $data["client"],
+            "vendeur" => $data["vendeur"],
+            "evenementId" => $data["evenementId"],
+            "prix" => $data["prix"],
+            "dateEvenement" => $dateEvenement
+        ]);
+        if ($ticket === NULL) {
+            return new JsonResponse(false);
+        } else {
+            return new JsonResponse(true);
+        }
+    }
+}
